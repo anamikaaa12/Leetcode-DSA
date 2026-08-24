de4c74ead0353dccefc4a0e8873e987a50d8d16c
anamikaaa12 <anamikasri76350@gmail.com>
2026-08-24 14:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (36.24%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -5,9 +5,8 @@ class Solution {
 
         }
 int[] dp= new int[n+1];
-dp[1]= 1;
-dp[2]=2;
-for(int i=3; i<=n; i++){
+dp[0]= dp[1]= 1;
+for(int i=2; i<=n; i++){
     dp[i]= dp[i-1]+ dp[i-2];
 }        
 return dp[n];
